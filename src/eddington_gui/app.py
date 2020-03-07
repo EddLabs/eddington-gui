c3a251aee816d6eb0eb4bedd379ecdcc1db46b98
Sagi Shadur <saroad2@gmail.com>
2020-03-07 16:36:53 +0200

    Add choose file selection

--- src/eddington_gui/app.py
+++ src/eddington_gui/app.py
@@ -8,6 +8,8 @@ from toga.style.pack import COLUMN, ROW, CENTER, FANTASY
 
 class EddingtonGUI(toga.App):
 
+    input_file_path: toga.TextInput
+
     def startup(self):
         """
         Construct and show the Toga application.
@@ -21,9 +23,21 @@ class EddingtonGUI(toga.App):
                                  style=Pack(text_align=CENTER, font_family=FANTASY, font_size=23))
         main_box.add(title_label)
 
+        file_box = toga.Box(style=Pack(direction=ROW))
+        file_box.add(toga.Label(text="Input file:"))
+        self.input_file_path = toga.TextInput(readonly=True, style=Pack(flex=1, padding_left=3, padding_right=3))
+        file_box.add(self.input_file_path)
+        file_box.add(toga.Button(label="Choose file", on_press=self.select_file))
+        main_box.add(file_box)
+
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
+
+    def select_file(self, widget):
+        input_file_path = self.main_window.open_file_dialog(title="Choose input file",
+                                                            multiselect=False)
+        self.input_file_path.value = input_file_path
 
 
 def main():
