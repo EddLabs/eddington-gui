1f94e78328e649a214fc6601f93c8ae679657219
Sagi Shadur <saroad2@gmail.com>
2020-08-10 22:14:16 +0300

    fix pylint and pydocstyle errors in util.py

--- src/eddington_gui/util.py
+++ src/eddington_gui/util.py
@@ -1,4 +1,8 @@
+"""Utility methods."""
+
+
 def value_or_none(value):
+    """Return string value if not empty. Otherwise, returns None."""
     if value == "":
         return None
     return value
