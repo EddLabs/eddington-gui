acf8f2b545889a719cb06349fdda9243cb597e39
Sagi Shadur <saroad2@gmail.com>
2020-08-10 22:02:43 +0300

    Fix pylint and pydocstyle errors in figure_window.py

--- src/eddington_gui/window/figure_window.py
+++ src/eddington_gui/window/figure_window.py
@@ -1,10 +1,18 @@
+"""Window that presents matplotlib figures."""
 import toga
 from matplotlib.figure import Figure
 from toga_chart import Chart
 
 
 class FigureWindow(toga.Window):
+    """
+    Window that contains a chart with a *matplotlib* figure.
+
+    This is made using toga.Chart widget.
+    """
+
     def __init__(self, figure: Figure):
+        """Initialize window."""
         super(FigureWindow, self).__init__(
             size=figure.get_size_inches() * figure.get_dpi()
         )
