9ef731fa227271ae37ee1eda8513405c3669b8b9
Sagi Shadur <saroad2@gmail.com>
2020-08-22 11:08:19 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
+from pathlib import Path
+
+import toml
 from setuptools import setup
 
-NAME = "eddington_gui"
+pyproject = Path(__file__).parent / "pyproject.toml"
+config = toml.load(pyproject)["tool"]["briefcase"]
+
+
 if __name__ == "__main__":
-    setup(name=NAME)
+    name = config["name"]
+    setup(name=name, install_requires=config["app"][name.replace("-", "_")]["requires"])
