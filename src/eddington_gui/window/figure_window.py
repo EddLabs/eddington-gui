a73892346ca86c07023743eb38435ce296ad755c
OhadNir9 <ohadnir@mail.tau.ac.il>
2020-08-31 10:57:30 +0300

    statue run

--- src/eddington_gui/window/figure_window.py
+++ src/eddington_gui/window/figure_window.py
@@ -13,6 +13,7 @@ class FigureWindow(toga.Window):  # pylint: disable=too-few-public-methods
 
     This is made using toga.Chart widget.
     """
+
     figure: Figure
 
     def __init__(self, figure: Figure):
@@ -20,15 +21,23 @@ class FigureWindow(toga.Window):  # pylint: disable=too-few-public-methods
         self.figure = figure
         super().__init__(size=(1.1, 1) * (figure.get_size_inches() * figure.get_dpi()))
         chart = Chart()
-        save_button=toga.Button(label="Save", style=Pack(flex=1), on_press=self.save_figure)
-        self.content = toga.Box(children=[save_button, chart], style=Pack(direction=ROW))
+        save_button = toga.Button(
+            label="Save", style=Pack(flex=1), on_press=self.save_figure
+        )
+        self.content = toga.Box(
+            children=[save_button, chart], style=Pack(direction=ROW)
+        )
         chart.draw(figure)
 
-    def save_figure(self,widget):
+    def save_figure(self, widget):  # pylint: disable=unused-argument
+        """Save file dialog."""
         try:
-            output_path=Path(self.save_file_dialog(title="Save Figure", suggested_filename="fig", file_types=["png"]))
+            output_path = Path(
+                self.save_file_dialog(
+                    title="Save Figure", suggested_filename="fig", file_types=["png"]
+                )
+            )
         except ValueError:
             return
 
-        self.figure.savefig(fname=output_path.with_suffix('.png'), format='png')
-
+        self.figure.savefig(fname=output_path.with_suffix(".png"), format="png")
