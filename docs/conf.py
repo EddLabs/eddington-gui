a0fba34b3da4e6fd4cd8a3953c827d205590f162
Sagi Shadur <saroad2@gmail.com>
2020-09-16 17:56:43 +0300

    Add background.rst to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, (Path(__file__).parent.parent / "src").absolute())
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.imgmath"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.imgmath", "sphinxcontrib.video"]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
