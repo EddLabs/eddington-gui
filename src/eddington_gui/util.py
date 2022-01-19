2d7529cbc52b3822bce23962ab44e51eb7e1c708
Sagi Shadur <saroad2@gmail.com>
2022-01-19 15:53:48 +0200

    Fix darglint errors in util.py

--- src/eddington_gui/util.py
+++ src/eddington_gui/util.py
@@ -1,8 +1,15 @@
 """Utility methods."""
 
 
-def value_or_none(value):
-    """Return string value if not empty. Otherwise, returns None."""
+def value_or_none(value: str):
+    """
+    Return string value if not empty. Otherwise, returns None.
+
+    :param value: a string value to convert into a None if its empty
+    :type value: str
+    :return: None or the value itself
+    :rtype: None or str
+    """
     if value.strip() == "":
         return None
     return value
