fc4a808001a4867d1f6af15d9025181670d54f5a
RPELLED <RPELLED@users.noreply.github.com>
2022-11-15 21:35:55 +0200

    fix typo

--- src/eddington_gui/boxes/fitting_function_box.py
+++ src/eddington_gui/boxes/fitting_function_box.py
@@ -72,8 +72,7 @@ class FittingFunctionBox(LineBox):  # pylint: disable=too-many-instance-attribut
     @property
     def fitting_function_state(self):
         """Set fit function state."""
-        return self.
-.value
+        return self.fitting_function_selection.value
 
     @property
     def on_fitting_function_load(self) -> Optional[Callable]:
